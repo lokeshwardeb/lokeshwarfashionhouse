541e515bf639946cf27a2c744e75ce142838dfec
lokeshwardeb <lokeshwardebdevelop@gmail.com>
2023-06-13 01:43:28 +0600

    added the server_error page

--- inc/conn.php
+++ inc/conn.php
@@ -12,19 +12,36 @@
   
 $conn = mysqli_connect($db_hostname, $db_username, $db_password, $database);
 
+// checking that if mysqli error occurs. It the mysqli database connect error occurs than it will follow the bellow code instractions and will process the code further
+
+
+if(mysqli_connect_error()){
+  // echo 'connect failed';
+  // header("location: ./install.php");
+  $_SESSION['server_error'] = 1;
+  header("location: ./server_error.php");
+}else{
+  // echo 'connected';
+  $_SESSION['server_error'] = 0;
+
+
+}
+// echo $_SESSION['server_error'];
+
 // for initializing the utf8 and the bangla font language support on mysql database
 mysqli_query($conn, "SET CHARACTER SET utf8");
 mysqli_query($conn, "SET SESSION collation_connection ='utf8_general_ci'");
 
 
-if($conn){
-    // echo 'connected with the database';
-}
-else{
-    echo 'Sorry ! Cannot connected with the database for this error =>'. mysqli_connect_error();
-    header('location: ../install.php');
+// if($conn){
+//     // echo 'connected with the database';
+// }
+// else{
+//     echo 'Sorry ! Cannot connected with the database for this error =>'. mysqli_connect_error();
+//     header('location: ../install.php');
 
-}
+// }
+
 
 // $select_conn = mysqli_connect($hostname, $username, $password);
 
