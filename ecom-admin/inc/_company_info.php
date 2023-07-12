b88069f3fdbb60d57ac615d7e96aa24890b35bee
lokeshwardeb <lokeshwardebdevelop@gmail.com>
2023-07-12 10:03:53 +0600

    fixed the ecom-admin loggedin admin user image

--- ecom-admin/inc/_company_info.php
+++ ecom-admin/inc/_company_info.php
@@ -1,5 +1,4 @@
 <?php 
-include 'inc/_error_reporting.php';
 
 // session_start();
 
@@ -7,28 +6,23 @@ include 'conn.php';
 
 $sql = 'SELECT * FROM `settings`';
 
-
-$result = @@mysqli_query($conn, $sql);
+$result = mysqli_query($conn, $sql);
 
 
 
 if ($result) {
-    if (@mysqli_num_rows($result) > 0) {
+    if (mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
             $id = $row['id'];
             $website_name = $row['website_name'];
             $website_description = $row['website_description'];
-            $website_contract_email = $row['website_contract_email'];
             $website_slogan = $row['website_slogan'];
-            $website_phone_no = $row['phone_no'];
-            $website_facebook_page = $row['website_facebook_page'];
             $aurthors_name = $row['authors_name'];
             $authors_email = $row['authors_email'];
             $company_name = $row['company_name'];
             $company_phone_no = $row['phone_no'];
             $website_logo = $row['logo_img_upload'];
             $login_page = $row['login_img'];
-            $users_login_upload = $row['users_login_img'];
 
        
             // echo 'the website name is' . $website_name;
@@ -38,15 +32,12 @@ if ($result) {
         $id = '';
         $website_name = '';
         $website_description = '';
-        $website_contract_email = '';
         $aurthors_name = '';
         $authors_email = '';
         $company_name = '';
         $company_phone_no = '';
-        $website_facebook_page = '';
     }
 }
-
 
 
 if(isset($_SESSION['username'])){
@@ -55,7 +46,7 @@ if(isset($_SESSION['username'])){
 
 
     $info_sql = "SELECT * FROM `admin_users` WHERE `username` = '$check_username';";
-$info_result = @mysqli_query($conn, $info_sql);
+$info_result = mysqli_query($conn, $info_sql);
 
 if ($info_result) {
     while ($row = mysqli_fetch_assoc($info_result)) {
@@ -79,59 +70,6 @@ if ($info_result) {
     $_SESSION['admin_photo'] = '';
 }
 
-}
-
-
-
-
-
-
-$info_sql = "SELECT * FROM `admin_users`   ";
-$info_result = @mysqli_query($conn, $info_sql);
-
-if ($info_result) {
-    while ($row = mysqli_fetch_assoc($info_result)) {
-        $check_id = $row['id'];
-        $admin_username = $row['username'];
-        $admin_email = $row['email'];
-        $profile_description = $row['admin_description'];
-        $phone_no = $row['admin_phone_no'];
-        $admin_photo = $row['admin_photo'];
-        $role = $row['admin_role'];
-        $_SESSION['admin_photo'] = 'uploaded_img/' . $admin_photo;
-    }
-} else {
-    $check_id = '';
-    $profile_username = '';
-    $email = '';
-    $profile_description = '';
-    $phone_no = '';
-    $admin_photo = '';
-    $role = '';
-    $_SESSION['admin_photo'] = '';
-}
-
-
-
-// cus users info
-
-$get_cus_username = $_SESSION['cus_username'];
-
-$sql_cus_users = "SELECT * FROM `cus_users` WHERE `cus_username` = '$get_cus_username'";
-
-$result_cus_users = mysqli_query($conn, $sql_cus_users);
-
-if($result_cus_users){
-    if(mysqli_num_rows($result_cus_users) > 0 ){
-while ($row = mysqli_fetch_assoc($result_cus_users)) {
-    $cus_username = $row['cus_username'];
-    $cus_desc = $row['cus_desc'];
-    $cus_email = $row['cus_email'];
-    $cus_phone_no = $row['cus_phone_no'];
-    $cus_photo = $row['cus_photo'];
-    $cus_joined_datatime = $row['cus_joined_datatime'];
-}
-    }
 }
 
 
